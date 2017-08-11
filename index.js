966b8b00a2e6c4f84bc998cdb24318977bffbe63
Brianna Greene <brianna.greene.designs@gmail.com>
2017-08-11 16:26:17 -0600

    solved toggle light problem

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 (function() {
   'use strict';
 
-  // YOUR CODE HERE
+  let stopButton = document.getElementById('stopButton')
+  let stopLight = document.getElementById('stopLight')
+  stopButton.addEventListener('click', function() {
+    stopLight.classList.toggle('stop')
+  })
+
+  let slowButton = document.getElementById('slowButton')
+  let slowLight = document.getElementById('slowLight')
+  slowButton.addEventListener('click', function() {
+    slowLight.classList.toggle('slow')
+  })
+
+  let goButton = document.getElementById('goButton')
+  let goLight = document.getElementById('goLight')
+  goButton.addEventListener('click', function() {
+    goLight.classList.toggle('go')
+  })
 })();
